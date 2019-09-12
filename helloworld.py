c88eb309df5d2c2b3ec57675909dead91e1138ac
mleee <mleee@umich.edu>
2019-09-11 23:11:04 -0400

    Lab file

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+from random import randint
+
+print ("Alice")
+
+a = randint(0, 100)
+b = randint(0, 100)
+
+print (a)
+print (b)
+print ('Sum = ' + str(a+b))
+print ('Average = ' + str((a+b)/2.0))
